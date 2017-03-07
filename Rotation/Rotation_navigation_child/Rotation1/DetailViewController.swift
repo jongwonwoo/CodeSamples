597af24ec4d97dd26bdc9f069e441cb2ae9eb0be
Jongwon Woo <jongwonwoo1@gmail.com>
2017-03-07 14:03:36 +0900

    Navigation Controller의 child view controller만 회전하게 만들기.

---
+++ Rotation/Rotation_navigation_child/Rotation1/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  Rotation1
+//
+//  Created by 1000985 on 07/03/2017.
+//  Copyright © 2017 jongwonwoo. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return [.portrait, .landscape]
+    }
+
+}
---
+++ Rotation/Rotation_navigation_child/Rotation1/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Rotation1
+//
+//  Created by 1000985 on 07/03/2017.
+//  Copyright © 2017 jongwonwoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+
+}
+
