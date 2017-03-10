79e8659ede94c49331b6c93a819c48ecdedfa2e2
Jongwon Woo <jongwonwoo1@gmail.com>
2017-03-10 13:46:16 +0900

    네비게이션은 portrait만 지원인데 B만 landscapeLeft만 지원  A portrait > B landscape > C. 근데 C가 landscape로 열리는 문제 수정.

---
+++ ViewController/Rotation/Rotation_navigation_portrait_child_only_landscape/Rotation1/DetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DetailViewController.swift
+//  Rotation1
+//
+//  Created by Jongwon Woo on 07/03/2017.
+//  Copyright © 2017 jongwonwoo. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if (self.supportedInterfaceOrientations == .landscapeLeft && UIDevice.current.orientation != .landscapeLeft)
+        {
+            let value = UIInterfaceOrientation.landscapeLeft.rawValue
+            UIDevice.current.setValue(value, forKey: "orientation")
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscapeLeft
+    }
+
+}
---
+++ ViewController/Rotation/Rotation_navigation_portrait_child_only_landscape/Rotation1/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Rotation1
+//
+//  Created by Jongwon Woo on 07/03/2017.
+//  Copyright © 2017 jongwonwoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if (self.supportedInterfaceOrientations == .portrait && UIDevice.current.orientation != .portrait)
+        {
+            let value = UIInterfaceOrientation.portrait.rawValue
+            UIDevice.current.setValue(value, forKey: "orientation")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+
+}
+
