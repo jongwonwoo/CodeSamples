579fcee185bdb097f547853984b7484da3b41b55
Jongwon Woo <jongwonwoo1@gmail.com>
2017-03-10 10:54:22 +0900

    detail vc를 landscapeLeft로 열기

---
+++ ViewController/Rotation/Rotation_navigation_child_force_landscape/Rotation1/MyNavigationController.swift
@@ -0,0 +1,42 @@
+//
+//  MyNavigationController.swift
+//  Rotation1
+//
+//  Created by Jongwon Woo on 07/03/2017.
+//  Copyright © 2017 jongwonwoo. All rights reserved.
+//
+
+import UIKit
+
+class MyNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    override var shouldAutorotate: Bool {
+        return (self.topViewController?.shouldAutorotate)!
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return (self.topViewController?.supportedInterfaceOrientations)!
+    }
+}
---
+++ ViewController/Rotation/Rotation_navigation_child_force_landscape/Rotation1/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Rotation1
+//
+//  Created by Jongwon Woo on 07/03/2017.
+//  Copyright © 2017 jongwonwoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return [.portrait]
+    }
+
+}
+
