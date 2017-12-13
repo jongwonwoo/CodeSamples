5040708efbc22e9f7cabbb765fee0e28e4a69fb6
Jongwon Woo <jongwonwoo1@gmail.com>
2017-12-13 13:43:14 +0900

    add navigation controller

--- Safe Area/SafeAreaSample/SafeAreaSample/NavigationRootViewController.swift
+++ Safe Area/SafeAreaSample/SafeAreaSample/NavigationRootViewController.swift
@@ -1,23 +1,21 @@
 //
-//  ViewController.swift
+//  NavigationRootViewController.swift
 //  SafeAreaSample
 //
-//  Created by Jongwon Woo on 12/12/2017.
+//  Created by Jongwon Woo on 13/12/2017.
 //  Copyright © 2017 jongwonwoo. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class NavigationRootViewController: UIViewController {
 
     @IBOutlet weak var contentView: UIView!
-    @IBOutlet weak var descriptionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        showInfo()
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +23,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
@@ -32,15 +31,4 @@ class ViewController: UIViewController {
         contentView.layer.borderWidth = 3.0
     }
 
-
-    func showInfo() {
-        if #available(iOS 11.0, *) {
-            descriptionLabel.text = "iOS 11"
-        } else {
-            descriptionLabel.text = "older version than iOS 11"
-        }
-    }
-    
-    @IBAction func unwindToVC(segue:UIStoryboardSegue) { }
 }
-
